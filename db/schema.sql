9c7545adb90c544e3b666481fe3ef7eef1e668ea
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-27 14:17:22 -0700

    fixed schema, added score

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- DROP DATABASE IF EXISTS friend-finder_db;
+DROP DATABASE IF EXISTS friend_finder_db;
 CREATE DATABASE friend_finder_db;
 USE friend_finder_db;
 
@@ -20,6 +20,7 @@ CREATE TABLE scores (
     id INT NOT NULL AUTO_INCREMENT,
 	question_id INT NOT NULL,
 	friend_id INT NOT NULL,
+	score INT NOT NULL,
 	FOREIGN KEY (question_id) REFERENCES questions(id),	
 	FOREIGN KEY (friend_id) REFERENCES friends(id),
     PRIMARY KEY(id)
