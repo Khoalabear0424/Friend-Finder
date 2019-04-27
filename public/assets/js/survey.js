63bd95b93558c49c4131946a96b6cfa59ad2debd
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-27 00:31:36 -0700

    retrieved responses

--- public/assets/js/survey.js
+++ public/assets/js/survey.js
@@ -15,15 +15,16 @@ $.ajax({
         var $label = $('<label>');
         var $select = $('<select>')
 
-        $label.attr('for', 'sel1');
+        $label.attr('for', 'sel' + questions[i].id);
         $label.text('Select an Option:')
         $select.attr('class', 'form-control')
-        $select.attr('id', questions[i].id)
+        $select.attr('id', 'questionID' + questions[i].id)
 
-        var allOptions = [1 + ' Strongly Disagrees', 2, 3, 4, 5 + ' Strongly Agree']
+        var allOptions = ["", 1 + ' Strongly Disagrees', 2, 3, 4, 5 + ' Strongly Agree']
 
         for (let i in allOptions) {
             var $option = $('<option>');
+            $option.attr('value', i)
             $option.text(allOptions[i]);
             $select.append($option);
         }
@@ -38,12 +39,19 @@ $.ajax({
 
 
 $('#user_data').submit(function (e) {
+    var responses = [];
+    for (let i = 0; i < $('select').length; i++) {
+        responses.push($('select').eq(i).val())
+    }
+    console.log(responses);
     e.preventDefault();
     $.ajax({
         url: '/find-friend-match',
         method: 'POST',
         data: {
             user_name: $("#user_name").val(),
+            user_image_link: $("#user_image_link").val(),
+            // responses: responses,
         }
     }).then(function (response) {
         console.log(response);
