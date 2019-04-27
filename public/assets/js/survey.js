a18553e9f133ac8fdcfdad7fbf1d21ad889bcdd6
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-26 23:47:28 -0700

    added dropdown

--- public/assets/js/survey.js
+++ public/assets/js/survey.js
@@ -1,19 +1,35 @@
 
-
 $.ajax({
     type: "GET",
     url: '/questions.json',
 }).then((questions) => {
     console.log(questions)
     for (let i in questions) {
+        console.log(questions[i].question)
         var $title = $('<h3>')
         $title.text('Question ' + (questions[i].id))
-        var $question = $('<p>');
-        $question.text(questions[i].question);
 
-        $('.questionsDisplay').append($title);
-        $('.questionsDisplay').append($question);
+        var $questionStatement = $('<p>');
+        $questionStatement.text(questions[i].question);
+
+        var $label = $('<label>');
+        var $select = $('<select>')
+
+        $label.attr('for', 'sel1');
+        $label.text('Select an Option:')
+        $select.attr('class', 'form-control')
+        $select.attr('id', questions[i].id)
+
+        for (let i = 1; i <= 5; i++) {
+            var $option = $('<option>');
+            $option.text(i);
+            $select.append($option);
+        }
+
+        var $questionDropDown = $('<form-group>');
+        $questionDropDown.append($label, $select)
+
+        $('.questionsDisplay').append($title, $questionStatement, $questionDropDown);
         $('.questionsDisplay').append('<br>');
     }
 });
-
