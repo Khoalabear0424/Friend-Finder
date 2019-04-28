d466fe484c3a770f41cfbc4eb4e9ec993bd83998
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-27 17:05:26 -0700

    wrote query to find least difference

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/questions.json', function (req, res) {
 
 app.post('/find-friend-match', function (req, res) {
     connection.query('INSERT INTO friends (name, picture_link) VALUES (?,?)', [req.body.user_name, req.body.user_image_link], function (error, results, fields) {
-
+        var current_user_id = results.insertId;
         var userScores = req.body.responses.map((score, index) => {
             return [index + 1, results.insertId, score]
         })
@@ -42,9 +42,16 @@ app.post('/find-friend-match', function (req, res) {
             if (error) res.send(error)
             else {
                 console.log(`Added questions scores `)
-                res.send(req.body)
             }
         });
+
+        connection.query('SELECT existing_name, SUM(score_difference) AS total_difference FROM (SELECT s1.question_id, f1.name AS existing_name, s1.score AS existing_score, n.name AS current_name, n.score AS current_score, ABS(s1.score-n.score) AS score_difference FROM scores s1 LEFT JOIN friends f1 ON s1.friend_id = f1.id LEFT JOIN (SELECT s2.question_id, f2.name, s2.score FROM scores s2 LEFT JOIN friends f2 ON s2.friend_id = f2.id WHERE s2.friend_id = ? ORDER BY s2.question_id ASC) n ON s1.question_id = n.question_id WHERE f1.id < ?) results GROUP BY existing_name ORDER BY total_difference ASC', [current_user_id, current_user_id], function (error, results, fields) {
+            if (error) res.send(error)
+            else {
+                console.log('Success on finding total difference')
+                res.send(results)
+            }
+        })
     })
 })
 
